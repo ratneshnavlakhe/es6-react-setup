7ba74d37e4c45fb1a5be4458dc66896295e1b0c1
Ratnesh Navlakhe <ratnesh@leftshift.io>
2016-07-07 16:12:23 +0530

    Learned about state of the component and constructor super keyword

--- App.js
+++ App.js
@@ -3,25 +3,31 @@ import ReactDOM from 'react-dom';
 
 // Make a class component.
 class App extends React.Component {
+  constructor() {
+    super(); // its gonna give the context of this in our component
+    this.state = {
+      txt: 'this is the state txt'
+    };
+  }
+
+  update(e) {
+    this.setState({ txt: e.target.value });
+  }
   render() {
-    let txt = this.props.txt;
-    return <h1>{txt}</h1>
+    return (
+      <div>
+        <input type="text"
+          onChange={this.update.bind(this)} />
+        <h1>{this.state.txt}</h1>
+      </div>
+    )
   }
 }
 
 //state less function component.
 //const App = () => <h1>Hello World</h1>
 
-App.propTypes = {
-  txt: React.PropTypes.string,
-  cat: React.PropTypes.number.isRequired
-}
-
-App.defaultProps = {
-  txt: 'this is the default txt'
-}
-
 ReactDOM.render(
-  <App cat={5} />,
+  <App />,
   document.getElementById('app')
 );
