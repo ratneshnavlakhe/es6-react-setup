c8b0dcd304491f08b8ec6c8a281485446f5c1719
Ratnesh Navlakhe <ratnesh@leftshift.io>
2016-07-07 18:11:25 +0530

    Mixins react component

--- App.js
+++ App.js
@@ -1,38 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// Make a class component.
-class App extends React.Component {
+
+let Mixin = InnerComponent => class extends React.Component {
   constructor() {
     super();
-    this.state = { increasing: false };
+    this.state = { val: 0 };
     this.update = this.update.bind(this);
   }
 
   update() {
-    ReactDOM.render(
-      <App val={this.props.val+1} />,
-      document.getElementById('app')
-    );
+    this.setState({ val: this.state.val + 1 })
   }
 
-  componentWillReceiveProps(nextProps) {
-    this.setState({ increasing: nextProps.val > this.props.val })
+  componentWillMount() {
+    console.log('Will mount')
   }
 
   render() {
-    console.log(this.state.increasing);
-    return <button onClick={this.update}>{this.props.val}</button>
+    return <InnerComponent
+      update={this.update}
+      {...this.state}
+      {...this.props} />
   }
-  shouldComponentUpdate(nextProps, nextState) {
-    //update only if passes the condition
-    return nextProps.val % 5 === 0;
-  }
-  componentDidUpdate(prevProps, prevState) {
-    console.log('prevProps', prevProps);
+  componentDidMount() {
+    console.log('mounted');
   }
 }
 
-App.defaultProps = { val: 0 }
+const Button = (props) => <button
+                          onClick={props.update}>
+                          {props.txt} - {props.val}
+                          </button>
+
+let ButtonMixed = Mixin(Button)
+// Make a class component.
+class App extends React.Component {
+  render() {
+    return (
+      <ButtonMixed txt="button"/>
+    );
+  }
+}
 
 export default App
