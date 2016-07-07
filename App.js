14c8b07b742fdf9cdea22a8330d10872790d7cde
Ratnesh Navlakhe <ratnesh@leftshift.io>
2016-07-07 15:51:02 +0530

    React render method require a single node to be rendered. It because each node is a separated function eg React.createElement('h1', null, 'Hello world');

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
-// class component.
-// class App extends React.Component {
-//   render() {
-//     return React.createElement('h1', null, 'Hello Guys');
-//   }
-// }
+// Make a class component.
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <b>bold</b>
+      </div>
+    )
+  }
+}
 
 //state less function component.
-const App = () => <h1>Hello World</h1>
+//const App = () => <h1>Hello World</h1>
 
 export default App
